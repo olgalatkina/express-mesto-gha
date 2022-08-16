039d6ad0504fe6ebfb42921b874456e8ebc24287
Olga Latkina <olga.latkina@gmail.com>
2022-08-16 23:21:36 +0700

    feat: handle "Page not found"

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const router = require('./routes');
+const { CodeError } = require('./constants');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -19,8 +24,8 @@ app.use((req, res, next) => {
 
 app.use(router);
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
+app.use("*", (req, res) => {
+  res.status(CodeError.NOT_FOUND).send({ message: "Страница не найдена" });
 });
 
 app.listen(PORT, () => {
