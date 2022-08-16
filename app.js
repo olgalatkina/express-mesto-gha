5c043ceaf6fade574339a5581b0e11a1993b2e7f
Olga Latkina <olga.latkina@gmail.com>
2022-08-17 01:18:43 +0700

    fix: linter

--- app.js
+++ app.js
@@ -7,13 +7,6 @@ const { CodeError } = require('./constants');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb', {
-  useNewUrlParser: true,
-});
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use((req, res, next) => {
   req.user = {
     _id: '62fb7b8cce30abcbaa5d4fe1'
@@ -22,10 +15,16 @@ app.use((req, res, next) => {
   next();
 });
 
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
-app.use("*", (req, res) => {
-  res.status(CodeError.NOT_FOUND).send({ message: "Страница не найдена" });
+app.use('*', (req, res) => {
+  res.status(CodeError.NOT_FOUND).send({ message: 'Страница не найдена' });
 });
 
 app.listen(PORT, () => {
